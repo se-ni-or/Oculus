6bb64129bf6914ca0de5a0944aab798bee14c7f1
senior <senior2508@mail.ru>
2023-03-23 23:50:22 +0700

    before js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-const btns = document.querySelectorAll(".tabs__btn-item")
-const tabsItems = document.querySelector(".tabs__items")
-
-btns.forEach((element, i)=>{
-    element.addEventListener("click", (event)=>{
-        btns.forEach((element)=>{
-            element.classList.remove('tabs__btn-item--active')
-        })
-        element.classList.add("tabs__btn-item--active")
-        const height = 318 * i
-        tabsItems.style.transform = `translateY(${-height}px)`
-    })
-})
+git
